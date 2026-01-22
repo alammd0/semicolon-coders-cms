06f93d31a471931c6bd0200b47116e2ba7e438ea
alammd0 <sonualam96314@gmail.com>
2026-01-22 15:37:09 +0530

    Strapi production ready

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,25 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {}
+      },
+      security: {
+        allowedTypes: [
+          "image/jpeg",
+          "image/png",
+          "image/webp",
+          "image/svg+xml",
+          "video/mp4",
+        ],
+        deniedTypes: [],
+      },
+    },
+  },
+});
